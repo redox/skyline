e9b3136ee15b5fe1dfb44bb9fe512e598a4a7c7c
Sylvain UTARD <sylvain.utard@gmail.com>
2014-10-21 09:27:45 +0200

    Add main, exiting 0 if OK

---
+++ src/analyzer/__main__.py
@@ -0,0 +1,27 @@
+import sys
+import json
+import algorithms
+from algorithm_exceptions import *
+
+if len(sys.argv) != 2:
+  print(("usage: %s /path/to/serie.json" % sys.argv[0]), file=sys.stderr)
+  sys.exit(1)
+
+with open(sys.argv[1]) as data_file: 
+  data = json.load(data_file)
+
+if data['serie'] is None:
+  print(("usage: %s /path/to/serie.json" % sys.argv[0]), file=sys.stderr)
+  sys.exit(2)
+
+try:
+  sys.exit(0 if algorithms.run_selected_algorithm(data['serie']) else 1)
+except TooShort:
+  print("Too short")
+  sys.exit(0)
+except Boring:
+  print("Boring")
+  sys.exit(0)
+except Stale:
+  print("Stale")
+  sys.exit(0)
